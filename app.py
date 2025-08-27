f05e5541365d72f42ea1d7b583377c6efca68aa6
yagi0308 <choodee@icloud.com>
2025-08-27 13:01:58 +0900

    ルーティング設定

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def hello():
     return "Hello, Flask!"
 
+@app.route("/guri")
+def guri():
+    return "Hello, guri!"
+
+@app.route("/gura")
+def gura():
+    return "Hello, gura!"
+
 if __name__ == "__main__":
     app.run(debug=True)
