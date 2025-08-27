f9deddc2cf0fa1a068321bc04da5cc58357ed62b
yagi0308 <choodee@icloud.com>
2025-08-27 14:38:13 +0900

    条件分岐

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route("/<name>")
+def home(name):
+    fruits = ["Apple", "Banana", "Orange", "Grapes"]  # リストを用意
+    return render_template("index.html", name=name, fruits=fruits)
 
 if __name__ == "__main__":
     app.run(debug=True)
