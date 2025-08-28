a024e73b2de9427c3051d4b39af9a3eeea610402
yagi0308 <choodee@icloud.com>
2025-08-28 15:58:47 +0900

    ログアウト実装

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ def login():
     return render_template("login.html")
 
 # --- ログアウト ---
-@app.route("/logout")
+@app.route("/logout", methods=["GET", "POST"])
 @login_required
 def logout():
     logout_user()
